7b47d7e7f4b32938f279f880f29a09203062002b
BlakePro <blakegt@gmail.com>
2019-07-29 23:03:35 -0500

    Update Sql.php
    Remove utilitites

--- src/Sql.php
+++ src/Sql.php
@@ -9,20 +9,6 @@ class Sql{
     $this->database_name = $this->key('name', $attr);
     $this->database_user = $this->key('user', $attr);
     $this->database_password = $this->key('password', $attr);
-    $this->encryption_key = $this->key('encryption_key', $attr);
-  }
-
-  public function encrypt($string){
-    return openssl_encrypt($string, "AES-128-ECB", $this->encryption_key);
-  }
-
-  public function decrypt($string){
-    return openssl_decrypt($string, "AES-128-ECB", $this->encryption_key);
-  }
-
-  public function key($key, $array){
-    if(!is_array($key) && is_array($array) && array_key_exists($key, $array))return $array[$key];
-    else return null;
   }
 
   public function query($args){
@@ -125,11 +111,11 @@ class Sql{
             $message = $exception->getMessage();
           }
         }
-  			$db = null;
-  		}catch(PDOException $exception) {
-  			$message = $exception->getMessage();
-  		}
+  	$db = null;
+  	}catch(PDOException $exception) {
+  		$message = $exception->getMessage();
   	}
+ 	}
   	return ['state' => $state,  'message' => $message, 'fetch' => $data_fetch, 'sql' => $this->query_print($sql_send, $params)];
   }
 
@@ -592,16 +578,5 @@ class Sql{
     }
     return $str_sql;
   }
-
-  public function in_string($search, $string){
-    $bool = strpos($string, $search);
-    if($bool === FALSE)return FALSE;
-    else return TRUE;
-  }
-
-  public function remove_str($string, $int = 1, $start = 0){
-    if(is_string($string) && $string != '')return trim(substr($string, $start, strlen($string)-$int));
-    else return $string;
-  }
-
+	
 }
