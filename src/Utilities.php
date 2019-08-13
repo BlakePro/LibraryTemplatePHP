73a174eebb62e4bba64d68b88af95df2df35b8da
BlakePro <blakegt@gmail.com>
2019-08-13 15:30:20 -0500

    Update Utilities.php
    Add function get_user_agent() and get_user_ip()

--- src/Utilities.php
+++ src/Utilities.php
@@ -74,6 +74,25 @@ class Utilities{
   function is_email($email) {
 	   return filter_var($email, FILTER_VALIDATE_EMAIL) ? TRUE : FALSE;
   }
+
+   //FUNCTION TO GET USER AGENT IP
+   function get_user_agent(){
+	return $this->key('HTTP_USER_AGENT', $_SERVER);
+   }
+
+//FUNCTION TO GET IP USER
+function get_user_ip(){
+	foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+        if (array_key_exists($key, $_SERVER) === TRUE){
+            foreach (explode(',', $_SERVER[$key]) as $ip){
+                $ip = trim($ip); 
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                    return $ip;
+                }
+            }
+        }
+    }
+}
 	
   public function parse($array, $key_select, $val_select, $name_table, $option_select = '', $return_option = TRUE, $empty_option = TRUE, $encrypt = FALSE){
     $return = [];
