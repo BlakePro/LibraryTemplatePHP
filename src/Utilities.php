67c2ab4d99c594a7290881806ab04123bf2abd43
BlakePro <blakegt@gmail.com>
2019-08-27 15:02:57 -0500

    Update Utilities.php

--- src/Utilities.php
+++ src/Utilities.php
@@ -109,19 +109,17 @@ class Utilities{
    	return $this->key('HTTP_ACCEPT_LANGUAGE', $_SERVER);
    }
 
-//FUNCTION TO GET IP USER
-function get_user_ip(){
-	foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
-        if (array_key_exists($key, $_SERVER) === TRUE){
-            foreach (explode(',', $_SERVER[$key]) as $ip){
-                $ip = trim($ip); 
-                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
-                    return $ip;
-                }
-            }
-        }
-    }
-}
+   //FUNCTION TO GET IP USER
+   function get_user_ip(){
+     foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+        if(array_key_exists($key, $_SERVER) === TRUE){
+	  foreach(explode(',', $_SERVER[$key]) as $ip){
+            $ip = trim($ip); 
+	    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false)return $ip;
+	  }
+	}
+     }
+   }
 	
   public function parse($array, $key_select, $val_select, $name_table, $option_select = '', $return_option = TRUE, $empty_option = TRUE, $encrypt = FALSE){
     $return = [];
