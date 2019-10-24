05385350261fb0b7e672148e2be26931b1145ad6
BlakePro <blakegt@gmail.com>
2019-10-23 23:16:27 -0500

    Create config.php
    New config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+  error_reporting(E_ALL);
+  ini_set("display_errors","On");
+  include 'vendor/autoload.php';
+
+  //CONFIG TEMPLATE
+  define('TEMPLATE_JSON', 'insertpagetemplate');
+  define('ENCRYPTION_KEY', 'insertkeyhere');
+  
+  //DATABASE PARAMETERS MYSQL
+  define('DB_HOST', '');
+  define('DB_USER', '');
+  define('DB_PASSWORD', '');
+  define('DB_NAME', '');
+  define('DB_PREFIX', '');
+  define('DB', DB_NAME.'.'.DB_PREFIX);
+
+  //TEMPLATE
+  $template = new blakepro\Template\Html();
+  $pdo = new blakepro\Template\Sql(['host' => DB_HOST, 'name' => '', 'user' => DB_USER, 'password' => DB_PASSWORD]);
+  $utilities = new blakepro\Template\Utilities(['encryption_key' => ENCRYPTION_KEY]);
