9db82864cee4d696efb4b8b2c5c1978990386ee3
BlakePro <blakegt@gmail.com>
2019-10-23 23:29:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-include 'config.php'
+//INCLUDE CONFIG FILE
+include 'config.php';
 
 //EXMAPLE TEMPLATE
 $html = 'Hello World';
+
+//ECHO TEMPLATE
 echo $template->template(TEMPLATE_JSON, ['html' => $html]);
