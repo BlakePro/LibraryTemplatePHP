ecc25cd064f20c1825420045b7a9d63b0ab2c090
BlakePro <blakegt@gmail.com>
2019-11-02 12:06:04 -0600

    Update installer.py

--- installer.py
+++ installer.py
@@ -9,5 +9,5 @@ print "\nLoading... \n"
 os.system("curl -s -o template.json https://raw.githubusercontent.com/BlakePro/Template/master/template.json")
 os.system("curl -s -o config.php https://raw.githubusercontent.com/BlakePro/Template/master/config.php")
 os.system("curl -s -o index.php https://raw.githubusercontent.com/BlakePro/Template/master/index.php")
-print "\n BlakePro Template installed succesfully \n \n File:\nindex.php \n Path:"
+print "\n BlakePro Template installed succesfully \n \nFile:\nindex.php \n Path:"
 os.system("pwd");
