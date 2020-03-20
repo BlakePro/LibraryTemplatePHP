1e7950dab9cbe5a37754d06092e50e3d8035e654
Cristian Yosafat Hernández Ruiz <blakegt@gmail.com>
2020-03-20 11:29:09 -0600

    Update installer.py
    Update print

--- installer.py
+++ installer.py
@@ -7,10 +7,10 @@ os.system("php composer-setup.php")
 os.system("php -r \"unlink('composer-setup.php');\"")
 os.system("php composer.phar require blakepro/template:dev-master")
 os.system("php -r \"unlink('installer.py');\"")
-print "\nLoading... \n"
+print ("\nLoading... \n")
 os.system("curl -s -o template.json https://raw.githubusercontent.com/BlakePro/Template/master/template.json")
 os.system("curl -s -o config.php https://raw.githubusercontent.com/BlakePro/Template/master/config.php")
 os.system("curl -s -o index.php https://raw.githubusercontent.com/BlakePro/Template/master/index.php")
-print "BlakePro Template installed succesfully \n \nFile index.php \n\nPath:\n"
+print ("BlakePro Template installed succesfully \n \nFile index.php \n\nPath:\n")
 os.system("pwd");
-print "\n"
+print ("\n")
